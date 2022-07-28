f9ddfaf189d6c69b5f5f9e2085cb0a1e50aad74d
dh58319 <dk58319@ajou.ac.kr>
2022-07-28 14:07:42 +0900

    FIX: alarm-priority

--- threads/thread.c
+++ threads/thread.c
@@ -255,7 +255,7 @@ void thread_unblock(struct thread *t)
 
 	old_level = intr_disable();
 	ASSERT(t->status == THREAD_BLOCKED);
-	list_push_back(&ready_list, &t->elem); // modify
+	list_insert_ordered(&ready_list, &t->elem, &cmp_priority, NULL);
 	t->status = THREAD_READY;
 	intr_set_level(old_level);
 }
@@ -320,7 +320,7 @@ void thread_yield(void)
 
 	old_level = intr_disable();
 	if (curr != idle_thread)
-		list_push_back(&ready_list, &curr->elem);
+		list_insert_ordered(&ready_list, &curr->elem, &cmp_priority, NULL);
 	do_schedule(THREAD_READY);
 	intr_set_level(old_level);
 }
@@ -329,6 +329,7 @@ void thread_yield(void)
 void thread_set_priority(int new_priority)
 {
 	thread_current()->priority = new_priority;
+	test_max_priority();
 }
 
 /* Returns the current thread's priority. */
@@ -671,4 +672,20 @@ bool cmp_priority(const struct list_elem *a, const struct list_elem *b, void *au
 	thread_a = list_entry(a, struct thread, elem);
 	thread_b = list_entry(b, struct thread, elem);
 	return thread_a->priority > (thread_b->priority) ? true : false;
+}
+void test_max_priority(void)
+{
+	if (list_empty(&ready_list))
+	{
+		return;
+	}
+
+	int run_priority = thread_current()->priority;
+	struct list_elem *e = list_begin(&ready_list);
+	struct thread *t = list_entry(e, struct thread, elem);
+
+	if (run_priority < t->priority)
+	{
+		thread_yield();
+	}
 }
